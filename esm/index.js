471be3a21b3bc0f854571ef729409e1f9ffac9f1
JnJ One <jnjsoft.one@gmail.com>
2025-03-13 16:06:26 +0900

    chore: build for publish

--- esm/index.js
+++ esm/index.js
@@ -1,1 +1,1 @@
-export{reqGet,reqPost,reqPatch,reqDelete,reqUpsert,reqGql}from"./request.js";export{Chrome,getProfileByEmail}from"./chrome.js";
+export{reqGet,reqPost,reqPatch,reqDelete,reqUpsert,reqGql}from"./request.js";export{Chrome,ChromeBasic,getProfileByEmail}from"./chrome.js";
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,3 @@
 export { reqGet, reqPost, reqPatch, reqDelete, reqUpsert, reqGql } from './request.js';
-export { Chrome, getProfileByEmail } from './chrome.js';
+export { Chrome, ChromeBasic, getProfileByEmail } from './chrome.js';
 //# sourceMappingURL=index.d.ts.map

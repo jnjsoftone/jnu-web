32935453a1a6e8ca5ce38f131f80201f733739d4
JnJ One <jnjsoft.one@gmail.com>
2025-03-13 16:05:39 +0900

    chromebasic -> index

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@
 // npm install @types/selenium-webdriver
 
 export { reqGet, reqPost, reqPatch, reqDelete, reqUpsert, reqGql } from './request.js';
-export { Chrome, getProfileByEmail } from './chrome.js';
+export { Chrome, ChromeBasic, getProfileByEmail } from './chrome.js';
